56c6e0cd55d8881d8a39fa205e3a5ee847da6cad
Phan Phương Phát <108333187+phuongphat1088@users.noreply.github.com>
2023-11-13 15:05:38 +0700

    fix search location

--- wordpress/wp-content/themes/jobscout/template-parts/header-form.php
+++ wordpress/wp-content/themes/jobscout/template-parts/header-form.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * Creating a custom job search form for homepage
@@ -8,14 +9,14 @@
  *
  * @package JobScout
  */
-$find_a_job_link = get_option( 'job_manager_jobs_page_id', 0 );
-$post_slug       = get_post_field( 'post_name', $find_a_job_link );
-$ed_job_category = get_option( 'job_manager_enable_categories' );  
+$find_a_job_link = get_option('job_manager_jobs_page_id', 0);
+$post_slug       = get_post_field('post_name', $find_a_job_link);
+$ed_job_category = get_option('job_manager_enable_categories');
 
-if( $post_slug ){
-    $action_page =  home_url( '/'. $post_slug );
-}else {
-    $action_page =  home_url( '/' );
+if ($post_slug) {
+  $action_page =  home_url('/' . $post_slug);
+} else {
+  $action_page =  home_url('/');
 }
 ?>
 
@@ -25,36 +26,44 @@ if( $post_slug ){
     <div class="search_jobs">
 
       <div class="search_keywords">
-        <label for="search_keywords"><?php esc_html_e( 'Keywords', 'jobscout' ); ?></label>
-        <input type="text" id="search_keywords" name="search_keywords" placeholder="<?php esc_attr_e( 'Keywords', 'jobscout' ); ?>">
+        <label for="search_keywords"><?php esc_html_e('Keywords', 'jobscout'); ?></label>
+        <input type="text" id="search_keywords" name="search_keywords" placeholder="<?php esc_attr_e('Keywords', 'jobscout'); ?>" value="<?php if (isset($_GET['search_keywords'])) {
+                                                                                                                                            echo $_GET['search_keywords'];
+                                                                                                                                          } ?>">
       </div>
 
       <div class="search_location">
         <select name="search_location" id="search_location">
-          <option selected>Location</option>
+          <option value="" <?php if (!isset($_GET['search_location'])) {
+                              echo 'selected';
+                            } ?>>Location</option>
           <?php
-            $all_job_locations = get_all_job_locations();
-            foreach ($all_job_locations as $location) {
-              echo '<option value="' . esc_html($location) . '">' . esc_html($location) . '</option>';
+          $all_job_locations = get_all_job_locations();
+          foreach ($all_job_locations as $location) {
+            $selected = '';
+            if (isset($_GET['search_location']) && esc_html($location) == $_GET['search_location']) {
+              $selected = 'selected';
             }
+            echo '<option value="' . esc_html($location) . '" ' . $selected . '>' . esc_html($location) . '</option>';
+          }
           ?>
         </select>
       </div>
-      
-      <?php if( $ed_job_category ){ ?>
-          <div class="search_categories custom_search_categories">
-            <label for="search_category"><?php esc_html_e( 'Job Category', 'jobscout' ); ?></label>
-            <select id="search_category" class="robo-search-category" name="search_category">
-            <option value=""><?php _e( 'Select Job Category', 'jobscout' ); ?></option>
-              <?php foreach ( get_job_listing_categories() as $jobcat ) : ?>
-                <option value="<?php echo esc_attr( $jobcat->term_id ); ?>"><?php echo esc_html( $jobcat->name ); ?></option>
-              <?php endforeach; ?>
-            </select>
-          </div>
+
+      <?php if ($ed_job_category) { ?>
+        <div class="search_categories custom_search_categories">
+          <label for="search_category"><?php esc_html_e('Job Category', 'jobscout'); ?></label>
+          <select id="search_category" class="robo-search-category" name="search_category">
+            <option value=""><?php _e('Select Job Category', 'jobscout'); ?></option>
+            <?php foreach (get_job_listing_categories() as $jobcat) : ?>
+              <option value="<?php echo esc_attr($jobcat->term_id); ?>"><?php echo esc_html($jobcat->name); ?></option>
+            <?php endforeach; ?>
+          </select>
+        </div>
       <?php } ?>
-      
+
       <div class="search_submit">
-        <input type="submit" value="<?php esc_attr_e( 'SEARCH JOB', 'jobscout'); ?>" />
+        <input type="submit" value="<?php esc_attr_e('SEARCH JOB', 'jobscout'); ?>" />
       </div>
 
     </div>
